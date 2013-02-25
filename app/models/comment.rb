27ee0698d6d6b986e57fc32b7fe573e1d498b438
Piotr Imbierowicz <piotr@k164-2.dwudziestolatka.ds.uni.wroc.pl>
2013-02-25 04:42:59 +0100

    Layout fix, comments order

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
-	default_scope order('created_at DESC')
+	default_scope order('created_at ASC')
 	attr_accessible :content, :note_id, :user_id
 	belongs_to :user
 	belongs_to :note
