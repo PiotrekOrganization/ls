eeabad695bec9dc6cb366f1c978cabc1eb6347ed
Piotr Imbierowicz <piotr@k164-2.dwudziestolatka.ds.uni.wroc.pl>
2013-02-26 04:19:19 +0100

    Place slug fix

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -13,6 +13,7 @@ class PlacesController < ApplicationController
 	def create
 		@place = Place.new(params[:place])
 		@place.user = current_user
+		@place.slug = @place.name.parameterize
 		if !@place.save
 			render new_subsciption_path
 			return
